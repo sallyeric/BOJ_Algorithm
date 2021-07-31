f450d27283bb85f1d3cc585996bf1940e3fe5da3
sallyeric <sally@DESKTOP-VEV7E4D.localdomain>
2021-07-31 18:57:25 +0900

    [미완] 애너그램 만들기

--- ETC/10808.cpp
+++ ETC/10808.cpp
@@ -1,4 +1,4 @@
-// 알파벳 개수
+// 알파벳 개수 (배열)
 #include <iostream>
 #include <vector>
 
---
+++ ETC/1919.cpp
@@ -0,0 +1,51 @@
+// 애너그램 만들기 (배열)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s1, s2;
+    cin >> s1;
+    cin >> s2;
+
+    vector<int> arr1(26);
+    vector<int> arr2(26);
+
+    for (char c = 'a'; c <= 'z'; c++)
+    {
+        int cnt = 0;
+        for (auto c1 : s1)
+        {
+            if (c == c1)
+            {
+                arr1[c - 'a'] += 1;
+            }
+        }
+        for (auto c2 : s2)
+        {
+            if (c == c2)
+            {
+                arr2[c - 'a'] += 1;
+            }
+        }
+    }
+
+    int answer;
+    for (int i = 0; i < 26; i++)
+    {
+        int tmp = arr1[i] - arr2[i];
+        if (tmp > 0)
+            answer += tmp;
+        if (tmp < 0)
+            tmp *= -1;
+        answer += tmp;
+    }
+
+    cout << answer;
+
+    return 0;
+}
