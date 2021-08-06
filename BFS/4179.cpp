02e6daf0b71c544e8e8a508c77cac9788b6dcd00
sallyeric <sally@DESKTOP-VEV7E4D.localdomain>
2021-08-07 00:58:50 +0900

    [완] 불!

--- BFS/4179.cpp
+++ BFS/4179.cpp
@@ -10,20 +10,84 @@ int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
 int R, C; // 행, 열
-int board[1002][1002];
-int jihun[1002][1002]; // 지훈 방문
-int fire[1002][1002];  // 불 방문
+string board[1002];
+int visit[1002][1002];
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    queue<pair<int, int>> Q;
+    queue<pair<int, int>> QJ;
+
+    cin >> R >> C;
+
+    for (int i = 0; i < R; i++)
+    {
+        cin >> board[i];
+    }
+
     for (int i = 0; i < R; i++)
     {
         for (int j = 0; j < C; j++)
         {
-            cin >> board[i][j];
+            if (board[i][j] == '.')
+                visit[i][j] = -1;
+            if (board[i][j] == 'F')
+                Q.push({i, j});
+            if (board[i][j] == 'J')
+                QJ.push({i, j});
         }
     }
+
+    // 불에 대한 BFS 처리
+    while (!Q.empty())
+    {
+        pair<int, int> cur = Q.front();
+        Q.pop();
+        for (int dir = 0; dir < 4; dir++)
+        {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            int tmp = visit[cur.X][cur.Y] + 1;
+            if (nx < 0 || nx > R || ny < 0 || ny > C)
+                continue;
+            if (visit[nx][ny] == -1 || (visit[nx][ny] > 0 && tmp < visit[nx][ny]))
+            {
+                visit[nx][ny] = tmp;
+                Q.push({nx, ny});
+            }
+        }
+    }
+
+    // 지훈이에 대한 BFS 처리
+    int ans = 0;
+    int fin_x = 0, fin_y = 0;
+    pair<int, int> cur = QJ.front();
+
+    while (!QJ.empty())
+    {
+        pair<int, int> cur = QJ.front();
+        QJ.pop();
+        for (int dir = 0; dir < 4; dir++)
+        {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            int tmp = visit[cur.X][cur.Y] + 1;
+            if (nx < 0 || nx >= R || ny < 0 || ny >= C)
+            {
+                cout << tmp;
+                return 0;
+            }
+            if (visit[nx][ny] == -1 || (visit[nx][ny] > 0 && tmp < visit[nx][ny]))
+            {
+                visit[nx][ny] = tmp;
+                ans = tmp;
+                QJ.push({nx, ny});
+            }
+        }
+    }
+
+    cout << "IMPOSSIBLE";
 }
