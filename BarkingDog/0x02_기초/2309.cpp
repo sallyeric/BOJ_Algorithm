3aa950af300a7f5119168beebccdc4c340bb604b
진승현 <sallyjin@mz.co.kr>
2022-01-23 19:45:04 +0900

    [미완] 일곱난장이

---
+++ BarkingDog/0x02_기초/2309.cpp
@@ -0,0 +1,13 @@
+// [BOJ 2309] 일곱 난쟁이
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int arr[9] = {};
+    for(int i = 0; i < 9; i++) cin >> arr[i];
+
+    
+}
